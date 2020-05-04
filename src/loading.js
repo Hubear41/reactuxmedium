8945fd6c8ae543676bf49e0daaf8c49df1ccdb17
Dennis Hu <ddh241@gmail.com>
2020-05-04 09:54:12 -0400

    create loading.js

---
+++ src/loading.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import FadeIn from "react-fade-in";
+import Lottie from "react-lottie";
+import ReactLoading from "react-loading";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function Loading() {
+  const [done, updateDone] = useState(undefined);
+
+  return <h1>hello world</h1>;
+}

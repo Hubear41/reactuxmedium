fbc38082d39556dab67d973af95af4963ad54a8a
Dennis Hu <ddh241@gmail.com>
2020-05-04 10:13:54 -0400

    added loading animations

--- src/loading.js
+++ src/loading.js
@@ -3,25 +3,39 @@ import FadeIn from "react-fade-in";
 import Lottie from "react-lottie";
 import ReactLoading from "react-loading";
 import "bootstrap/dist/css/bootstrap.css";
-import * as legoData from "./410-lego-loader.json";
+import * as legoData from "./loading_animations/410-lego-loader.json";
+import * as doneData from "./loading_animations/433-checked-done.json";
 
 const defaultOptions = {
   loop: true,
   autoplay: true,
   animationData: legoData.default,
-  renderSettings: {
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+const defaultOptions2 = {
+  loop: false,
+  autoplay: true,
+  animationData: doneData.default,
+  rendererSettings: {
     preserveAspectRatio: "xMidYMid slice",
   },
 };
 
 export default function Loading() {
   const [done, updateDone] = useState(false);
+  const [loading, updateLoading] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("https://jsonplaceholder.typicode.com/posts")
         .then((res) => res.json())
-        .then((json) => updateDone(true));
+        .then((json) => {
+          updateLoading(true);
+          setTimeout(() => updateDone(true), 1000);
+        });
     }, 1800);
   }, []);
 
@@ -31,7 +45,11 @@ export default function Loading() {
         <FadeIn>
           <div className="d-flex justify-content-center align-items-center">
             <h1>fetching pizza</h1>
-            <Lottie options={defaultOptions} height={120} width={120} />
+            {!loading ? (
+              <Lottie options={defaultOptions} height={120} width={120} />
+            ) : (
+              <Lottie options={defaultOptions2} height={120} width={120} />
+            )}
           </div>
         </FadeIn>
       ) : (
